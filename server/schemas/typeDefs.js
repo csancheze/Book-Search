29d6bedc94c496bf387dc6ae3ebba1b590e540ea
Cesar San <cesanchezesc@gmail.com>
2022-04-25 11:58:54 -0500

    first attempt as typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  type User {
+    _id: ID
+    username:String
+    email:String
+    password: String
+    savedBooks: [Book]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user(_id: ID, username: String): User
+  }
+
+  type Mutation {
+    createUser(username:String!, email:String!, password: String!): Auth
+    login(username:String, email:String, password: String!): Auth
+    saveBook(bookId: ID!): User
+    deleteBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
