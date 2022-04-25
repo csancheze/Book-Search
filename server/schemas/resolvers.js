a1e4569a67b54f18ff53cba5dbb6c36a5a59224c
Cesar San <cesanchezesc@gmail.com>
2022-04-25 11:31:27 -0500

    Added schema folder files and worked on resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,50 @@
+const { Book, User } = require('../models')
+
+const resolvers = {
+    Query: {
+        user: async ( parent, {user = null, _id, username}) => {
+            return User.findOne({
+                $or: [{ _id: user ? user._id : _id}, { username: username }],
+              });
+        },
+
+    },
+    Mutation: {
+        createUser: async (parent, args) => {
+            const user =  await User.create(args);
+            return user
+        },
+        login: async (parent, args) => {
+           
+            const user = await User.findOne({ $or: [{ username: args.username }, { email: args.email }]});
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(args.password);
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            console.log(user);
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {savedBooks: args}},
+                    {new: true}
+                );
+                return updatedUser
+        },
+        deleteBook: async (parent, args, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: args.bookId } } },
+                { new: true }
+            );
+            return updatedUser
+        },
+    },
+}
+
+module.exports = resolvers
